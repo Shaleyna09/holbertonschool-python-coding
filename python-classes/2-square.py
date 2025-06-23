12f24e7e3938f7d2e87907248096da353cd87df2
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-23 20:54:43 +0400

    Write a public instance method

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This module defines a class Square """
+
+
+class Square:
+    """ This class defines a square by its size and returns its area"""
+    def __init__(self, size=0):
+        """ Initialize the size """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ This function returns the area of the square. """
+        return self.__size ** 2
