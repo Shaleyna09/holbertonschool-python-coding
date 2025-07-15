8ddf5a9200e8163a498e96fa4c065c3959373a5b
Shaleyna09 <shaleynarungasamy@gmail.com>
2025-07-15 13:20:06 +0400

    frghyjuiop[

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-""" This module includes a Square class """
-
 
 class Square:
-    """ This class defines a square by its size."""
-
     def __init__(self, size):
-        """ Initialize a new square with a given size."""
         self.__size = size
