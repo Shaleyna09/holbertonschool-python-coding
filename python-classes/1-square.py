9ae1ce5b3389e8eaa5590ab59487939866f03d8a
Shaleyna09 <shaleynarungasamy@gmail.com>
2025-07-16 09:39:37 +0400

    sdfghjkl;'

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,6 +5,7 @@
 class Square:
 
     """ This class defines a square by its size."""
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
